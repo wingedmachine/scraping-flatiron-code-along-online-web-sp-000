8852fdbfbab7c9f74930f4a290a5a43b4e530714
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-24 15:45:21 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,12 @@
+class Course
+  attr_accessor :title, :schedule, :description
 
+    COURSES = []
 
+    def initialize(title, schedule, description)
+      @title = title
+      @schedule = schedule
+      @description = description
+      COURSES << self
+    end
+end
