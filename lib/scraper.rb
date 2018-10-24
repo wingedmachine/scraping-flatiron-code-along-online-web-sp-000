c712e0c921c61e99103aec3fb6c01e3716003636
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-24 15:59:12 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ require_relative './course.rb'
 
 class Scraper
   def get_page
-
+    doc = nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
 
   def get_courses
