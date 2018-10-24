e8833e6a549fe9eb7bca19709c0540c356d92eed
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-24 17:24:07 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,9 @@ require_relative './course.rb'
 
 class Scraper
   def get_page
-    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io" \
+                              "/site-for-scraping/courses"))
+    binding.pry
   end
 
   def get_courses
