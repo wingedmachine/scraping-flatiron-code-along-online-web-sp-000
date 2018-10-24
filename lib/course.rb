7377f6ba999b8d546372d849da1eb78902482359
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-24 17:31:23 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,15 @@
+class Course
+  attr_accessor :title, :schedule, :description
 
+  COURSES = []
+  def self.all
+    COURSES
+  end
+  def self.reset_all
+    COURSES.clear
+  end
 
+  def initialize
+    COURSES << self
+  end
+end
